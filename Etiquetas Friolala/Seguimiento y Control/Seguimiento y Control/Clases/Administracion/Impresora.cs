d8150794a79755598e304ca7473059cbe4c83cca
desarrollocorrales <desarrollo@loscorrales.com.mx>
2014-01-17 11:21:57 -0600

    Cambio en la ruta
    Se cambio la ruta de los archivos a la raiz

--- Etiquetas Friolala/Seguimiento y Control/Seguimiento y Control/Clases/Administracion/Impresora.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Seguimiento_y_Control.Clases.Administracion
-{
-    public class Impresora
-    {
-        public int ID { get; set; }
-        public string NombreImpresora { get; set; }
-    }
-}
